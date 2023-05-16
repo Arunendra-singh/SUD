027112c2b10dd000899d425126cc4563a9e97693
Arunendra <arunendra.sh@gmail.com>
2023-05-16 10:04:56 +0530

    Updated files

--- js/main.js
+++ js/main.js
@@ -1,50 +1,43 @@
 $(document).ready(function() {
     //product selection page PlanBox
 
-
-    // tab content
-    $(".tab_content").hide();
-    $(".tab_content:first").show();
-
-    /* if in tab mode */
-    $("ul.tabs li").click(function() {
-
-        $(".tab_content").hide();
-        var activeTab = $(this).attr("rel");
-        $("#" + activeTab).fadeIn();
-
-        $("ul.tabs li").removeClass("active");
-        $(this).addClass("active");
-
-    });
-
-    $('ul.tabs li').last().addClass("tab_last");
-
-
-    /*******************************************/
-
     //for card selection on hover 
 
     $(".card").hover(
         function() {
             $(".card").not(this).removeClass('cardBlueActive');
             $(this).toggleClass('cardBlueActive');
-
-            if ($('.PlanCntnDiv .card').hasClass("cardBlueActive")) {
-                $(this).nextsibling().addClass('active');
-                $(this).parent().parent().addClass('ful_height');
-
-            } else {
-                $(this).nextsibling().removeClass('active');
-                $(this).parent().parent().removeClass('ful_height');
-            }
         }
     );
+
+    $(".card").hover(function() {
+        if ($('.PlanCntnDiv .card').hasClass("cardBlueActive")) {
+            $(this).removeClass("active");
+            $(this).find(".WhiteImg").addClass("imgshow");
+
+            $(this).find(".ColorImg").addClass("imghide");
+
+        } else {
+            $(this).find(".WhiteImg").removeClass("imgshow");
+
+            $(this).find(".ColorImg").removeClass("imghide");
+        }
+
+    });
+
+    // for clicking on card 
+    $(".card").click(function() {
+        $(".card").not(this).removeClass('active');
+        $(this).addClass('active');
+
+    });
 
     $(".card .PlanKnowMore").click(function() {
         $(".card .PlanKnowMore").not(this).parent(".card").removeClass("cardBlueActive");
         $(this).parents(".card").toggleClass("cardBlueActive");
     });
+
+
 
 })
 
